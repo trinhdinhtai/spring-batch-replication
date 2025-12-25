719521571cd1d586011cd0c81d887a228849453d
trinhdinhtai <taitd153.dev@gmail.com>
2025-12-25 20:18:47 +0700

    feat: initial

---
+++ src/test/java/dev/taitd/replication/ReplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.taitd.replication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
